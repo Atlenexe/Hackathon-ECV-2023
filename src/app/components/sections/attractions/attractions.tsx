41b9756f88edda2711ad515e1e9a82bbfb1db77e
Atlenexe <hugo.mandou@gmail.com>
2023-10-26 11:58:31 +0200

    Bof

---
+++ src/app/components/sections/attractions/attractions.tsx
@@ -0,0 +1,37 @@
+import stylesSection from './../sections.module.css'
+import styles from './attractions.module.css'
+
+export default function Attractions() {
+    const sectionClasses = `${stylesSection.section} ${stylesSection.padding}`;
+
+    return (
+        <section className={sectionClasses}>
+            <h2>Attractions</h2>
+            <ul className={styles.attractions}>
+                <li>
+                    <ul>
+                        <li>
+                            <img src="/img/aquaponies.png" alt="" />
+                            <div>
+                                <p>Viens rencontrer Bloom-e & écoute la voix de la nature</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="/img/aquaponies.png" alt="" />
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <li>
+                            <img src="/img/aquaponies.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="/img/aquaponies.png" alt="" />
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+    )
+}
--- src/app/components/sections/sections.tsx
+++ src/app/components/sections/sections.tsx
@@ -1,10 +1,12 @@
 import Univers from './univers/univers'
+import Attractions from './attractions/attractions'
 import styles from './sections.module.css'
 
 export default function Sections() {
     return (
         <div className={styles.container}>
             <Univers />
+            <Attractions />
         </div>
     )
 }
